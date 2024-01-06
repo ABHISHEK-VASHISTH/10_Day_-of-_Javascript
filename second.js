b29f5ac7d5c78392b1998024ed5d1c25286c56ab
Abhishek Vashisth <100755099+ABHISHEK-VASHISTH@users.noreply.github.com>
2024-01-06 13:45:39 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,12 @@
+//Here we wil going to study about the variables in Javascript
+//We have 8 types of data types:=
+/*  String
+    Intiger
+    Bigint
+    Undefined
+    Null
+    Symbol
+    Object
+    Boolean
+ */
+console.log("")
