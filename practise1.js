f8167238359bd1ae352e69c446bdf02b8186d589
Abhishek Vashisth <100755099+ABHISHEK-VASHISTH@users.noreply.github.com>
2024-01-08 21:55:12 +0530

    Add files via upload

---
+++ practise1.js
@@ -0,0 +1,11 @@
+//You have given a id and its information make an object and retrive the tagLine out of it:-
+
+const id={
+    name: "Abhishek Vashisth",
+    isFollow: true,
+    Followers:589,
+    Following:234,
+    tagLine: "Heart of a lion, Mindset of a wolf",
+}
+
+console.log(id.tagLine);
