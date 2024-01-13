84163a16de60f5c707e4344d28f626a066efd719
Abhishek Vashisth <100755099+ABHISHEK-VASHISTH@users.noreply.github.com>
2024-01-13 21:17:11 +0530

    Add files via upload

---
+++ practise2.js
@@ -0,0 +1,8 @@
+// here we have to take the number from the user and give the result as the number is multiple of 5 or not.
+number=prompt("enter the number you want to check:- ");//prompt help us to take inputs and alert help us to display a message on the screen.
+ if(number%5===0){
+    console.log("yes it is divisible by 5");
+ }
+ else{
+    console.log("Sorry, it is not divisible by 5");
+ }
