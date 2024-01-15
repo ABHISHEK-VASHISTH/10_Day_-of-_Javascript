b45bd051d44211a39b80ff89d0312053f6940cc8
Abhishek Vashisth <100755099+ABHISHEK-VASHISTH@users.noreply.github.com>
2024-01-16 00:04:07 +0530

    Update fifth.js

--- fifth.js
+++ fifth.js
@@ -1,7 +1,47 @@
 //Loops and strings:-
 /*
     Loops:-
-     > 
-     >
-     >
+     > For loop
+     > While loop
+     > do....while loop
  */
+//For Loop
+for (initialization; condition; iteration) {
+    // code to be executed
+  }
+  for (let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+//While Loop
+while (condition) {
+    // code to be executed
+  }
+var i = 0;
+  while (i < 5) {
+    console.log(i);
+    i++;
+  }
+//Do...while Loop
+do {
+    // code to be executed
+  } while (condition);
+  var i = 0;
+  do {
+    console.log(i);
+    i++;
+  } while (i < 5);
+//We have some loop control statements as well those are:-
+//  > Break :- This helps the user to come out of a loop.
+//  > Continue :- This helps the user to skip a perticular iteration.
+//You can understand the concept more clearly by this example:-
+for (let i = 0; i < 5; i++) {
+    if (i === 2) {
+      continue; // Skip iteration when i is 2
+    }
+    if (i === 4) {
+      break; // Exit the loop when i is 4
+    }
+    console.log(i);
+  }
+  
+/*                                               I hope I was able to make you understand the concept of loops in Javascript                                                 */
