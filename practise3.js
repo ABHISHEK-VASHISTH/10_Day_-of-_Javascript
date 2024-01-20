500929d3aee9911a23dd3dbd287cf559a085c865
Abhishek Vashisth <100755099+ABHISHEK-VASHISTH@users.noreply.github.com>
2024-01-21 01:39:37 +0530

    Add files via upload

---
+++ practise3.js
@@ -0,0 +1,18 @@
+let number=0;
+for(let i=0;i<=100;i++){
+    if(i%2==0){
+        console.log(i);
+        number++;
+    }
+}
+console.log("total: ",number);
+
+//ask the user weather he can guess the right number or not:-
+ let guessnum=35;
+ let usernumber=prompt("guess the number= ");
+
+while(usernumber!==guessnum){
+    console.log("RETRY");
+
+ }
+console.log("you won the match")
