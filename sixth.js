f016c4b85adf986726484ea73aa9646de4808e13
Abhishek Vashisth <100755099+ABHISHEK-VASHISTH@users.noreply.github.com>
2024-01-26 21:51:09 +0530

    Update sixth.js

--- sixth.js
+++ sixth.js
@@ -1,3 +1,2 @@
 // Here we are going to study the concept of ____ in javascript
-
 /*                                               I hope I was able to make you understand the concept of loops in Javascript                                                 */
