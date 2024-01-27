b00b477048f029af4b69cb72ffbc0062b5127eee
Abhishek Vashisth <100755099+ABHISHEK-VASHISTH@users.noreply.github.com>
2024-01-28 00:52:10 +0530

    Update sixth.js

--- sixth.js
+++ sixth.js
@@ -28,6 +28,16 @@ let str3=str1+str2;
 console.log(str3);
 
 //string methods
+//for upper care printing:-
+let stri ="hello";
+console.log(stri.toUpperCase());
+//for lower case:-
+let strn="    HELLO    ";
+console.log(strn.toLowerCase());
+//trim
+console.log(strn.trim()); //removes the spaces
+//there are various other methods in string u can search them out on internet
+
 
 
 /*                                               I hope I was able to make you understand the concept of loops in Javascript                                                 */
