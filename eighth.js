024a85c21db403f235d159e3728b92829a8f965b
Abhishek Vashisth <100755099+ABHISHEK-VASHISTH@users.noreply.github.com>
2024-02-11 23:01:10 +0530

    Update eighth.js

--- eighth.js
+++ eighth.js
@@ -1,4 +1,6 @@
 //Here we are going to learn about functions in javascript:-
 
 
+
+
 /*                                               I hope I was able to make you understand the concept of variables in Javascript                                                 */
